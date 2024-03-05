5f44c9ce85f02552278c8b9dd39d8e65c7183fb3
Edgar Fenollar <edgarfenollarcanto@gmail.com>
2024-03-05 13:11:55 +0100

    Subo los siguientes 5 test

--- src/es/iesmz/EmpleadoBRTest.java
+++ src/es/iesmz/EmpleadoBRTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmpleadoBRTest {
+    // 1 Commit
     @Test
     public void calculaSalarioNeto1(){
         assertEquals(1640, EmpleadoBR.calculaSalarioNeto(2000));
@@ -29,7 +30,7 @@ class EmpleadoBRTest {
     public void calculaSalarioNeto5(){
         assertEquals(840, EmpleadoBR.calculaSalarioNeto(1000), 0.01);
     }
-
+    // 2 Commit
     @Test
     public void calculaSalarioNeto6(){
         assertEquals(999, EmpleadoBR.calculaSalarioNeto(999));
@@ -53,5 +54,30 @@ class EmpleadoBRTest {
     @Test
     public void calculaSalarioBruto1(){
         assertEquals(1360, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.vendedor, 2000, 8));
+    }
+    // 3 Commit
+    @Test
+    public void calculaSalarioBruto2(){
+        assertEquals(1260, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.vendedor, 1500, 3));
+    }
+
+    @Test
+    public void calculaSalarioBruto3(){
+        assertEquals(1100, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.vendedor, 1499.99f, 0));
+    }
+
+    @Test
+    public void calculaSalarioBruto4(){
+        assertEquals(1760, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encargado, 1250, 8));
+    }
+
+    @Test
+    public void calculaSalarioBruto5(){
+        assertEquals(1600, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encargado, 1000, 0));
+    }
+
+    @Test
+    public void calculaSalarioBruto6(){
+        assertEquals(1560, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encargado, 999.99f, 3));
     }
 }
